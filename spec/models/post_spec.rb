a64bf7b2c00483d8e912dadc3a39500536853b5b
Katherine G. Pe <katherinepe@me.com>
2016-09-07 22:42:49 +0800

    Post validation

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,25 @@ require 'spec_helper'
 
 describe Post, type: :model do
 
+  subject { described_class.new(title: 'A title', body: 'A body') }
+
+  context 'with valid attributes' do
+    it 'is valid' do
+      expect(subject).to be_valid
+    end
+  end
+
+  context 'without a title' do
+    it 'returns errors on title' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context 'without a body' do
+    it 'returns errors on body' do
+      subject.body = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
